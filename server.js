95b6ecca834ddc40fd946fc278eadb683e5fe764
Alex <ahawknj15@gmail.com>
2019-09-16 20:10:22 -0400

    fixed minor bugs in server.js and now app is running on local host with mongod and showing hello world

--- server.js
+++ server.js
@@ -14,16 +14,21 @@ const PORT = process.env.PORT || 3000;
 
 // ===  DATABASE === //
 const MONGODB_URI = process.env.MONGODB_URI;
-mongoose.connect(MONGODB_URI ,  { useNewUrlParser: true});
+mongoose.connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
 //depreciation warnings
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 //error / success
 db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
-db.on('connected', () => console.log('mongo connected: ', PROJECT3_DB));
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
 db.on('disconnected', () => console.log('mongo disconnected'));
+
+// === ROUTES === //
+app.get('/', (req, res) => {
+  res.send('hello world')
+});
 
 // === LISTENER === //
 app.listen(PORT, () => {
   console.log('listening on port ', PORT);
-}
+});
