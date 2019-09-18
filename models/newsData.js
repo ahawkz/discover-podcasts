9817a553b6c019ba92a0a35f6444287e0d50a34b
Alex <ahawknj15@gmail.com>
2019-09-18 18:42:38 -0400

    re-seeded data for heroku

--- models/newsData.js
+++ models/newsData.js
@@ -1,6 +1,6 @@
 const manyNews = [
   {
-    title: 'Ellen DeGeneres Introduces New Rescue Puppy',
+    headline: 'Ellen DeGeneres Introduces New Rescue Puppy',
     byline: 'Kelli Bender',
     url: 'https://people.com/pets/ellen-degeneres-adopts-rescue-dog-puppy/'
   }
